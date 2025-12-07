f22a716721fe44ed442cba8a2e9c33d372d30edc
kkoehler4070 <koehler.62@gmail.com>
2025-12-06 22:05:52 -0500

    Create board_tests.py
    Board unit tests

---
+++ tests/board_tests.py
@@ -0,0 +1,28 @@
+import unittest
+import chess
+
+def perft(board, depth):
+        """Count leaf nodes at a given depth."""
+        if depth == 0:
+            return 1
+        
+        moves = board.generate_legal_moves()
+        if depth == 1:
+            return len(moves)
+        
+        count = 0
+        for move in moves:
+            undo_info = board.make_move(move)
+            count += perft(board, depth - 1)
+            board.unmake_move(move, undo_info)
+        
+        return count
+
+class TestBoard(unittest.TestCase):
+    def TestDepth(self):
+        board = chess.Board()
+        self.assertEqual(perft(board,1),20)
+        self.assertEqual(perft(board,2),400)
+        self.assertEqual(perft(board,3),8902)
+        self.assertEqual(perft(board,4),197281)
+    
