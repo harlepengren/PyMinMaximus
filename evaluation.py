bba9f6bd0ea248865e070cad595db6cf04e1f1e3
kkoehler4070 <koehler.62@gmail.com>
2025-12-14 06:30:39 -0500

    Update evaluation.py
    Added piece-square tables to evaluate the strength of a particular space for a piece.

--- evaluation.py
+++ evaluation.py
@@ -10,7 +10,127 @@ class Evaluator:
             QUEEN: 900,
             KING: 20000
         }
+
+        # Piece-square tables for middlegame
+        self.setup_piece_square_tables()
+
+    def setup_piece_square_tables(self):
+        """
+        Set up piece-square tables.
+        Values are from White's perspective (higher = better for white).
+        Tables are stored with rank 0 = rank 1, rank 7 = rank 8.
+        """
+        
+        # Pawn table - encourage center control and advancement
+        self.pawn_table = [
+            [  0,   0,   0,   0,   0,   0,   0,   0],  # Rank 1
+            [ 50,  50,  50,  50,  50,  50,  50,  50],  # Rank 2
+            [ 10,  10,  20,  30,  30,  20,  10,  10],  # Rank 3
+            [  5,   5,  10,  25,  25,  10,   5,   5],  # Rank 4
+            [  0,   0,   0,  20,  20,   0,   0,   0],  # Rank 5
+            [  5,  -5, -10,   0,   0, -10,  -5,   5],  # Rank 6
+            [  5,  10,  10, -20, -20,  10,  10,   5],  # Rank 7
+            [  0,   0,   0,   0,   0,   0,   0,   0]   # Rank 8
+        ]
+        
+        # Knight table - encourage center control
+        self.knight_table = [
+            [-50, -40, -30, -30, -30, -30, -40, -50],
+            [-40, -20,   0,   0,   0,   0, -20, -40],
+            [-30,   0,  10,  15,  15,  10,   0, -30],
+            [-30,   5,  15,  20,  20,  15,   5, -30],
+            [-30,   0,  15,  20,  20,  15,   0, -30],
+            [-30,   5,  10,  15,  15,  10,   5, -30],
+            [-40, -20,   0,   5,   5,   0, -20, -40],
+            [-50, -40, -30, -30, -30, -30, -40, -50]
+        ]
+        
+        # Bishop table - encourage long diagonals
+        self.bishop_table = [
+            [-20, -10, -10, -10, -10, -10, -10, -20],
+            [-10,   0,   0,   0,   0,   0,   0, -10],
+            [-10,   0,   5,  10,  10,   5,   0, -10],
+            [-10,   5,   5,  10,  10,   5,   5, -10],
+            [-10,   0,  10,  10,  10,  10,   0, -10],
+            [-10,  10,  10,  10,  10,  10,  10, -10],
+            [-10,   5,   0,   0,   0,   0,   5, -10],
+            [-20, -10, -10, -10, -10, -10, -10, -20]
+        ]
+        
+        # Rook table - encourage open files and 7th rank
+        self.rook_table = [
+            [  0,   0,   0,   0,   0,   0,   0,   0],
+            [  5,  10,  10,  10,  10,  10,  10,   5],
+            [ -5,   0,   0,   0,   0,   0,   0,  -5],
+            [ -5,   0,   0,   0,   0,   0,   0,  -5],
+            [ -5,   0,   0,   0,   0,   0,   0,  -5],
+            [ -5,   0,   0,   0,   0,   0,   0,  -5],
+            [ -5,   0,   0,   0,   0,   0,   0,  -5],
+            [  0,   0,   0,   5,   5,   0,   0,   0]
+        ]
+        
+        # Queen table - slight center preference, stay back early
+        self.queen_table = [
+            [-20, -10, -10,  -5,  -5, -10, -10, -20],
+            [-10,   0,   0,   0,   0,   0,   0, -10],
+            [-10,   0,   5,   5,   5,   5,   0, -10],
+            [ -5,   0,   5,   5,   5,   5,   0,  -5],
+            [  0,   0,   5,   5,   5,   5,   0,  -5],
+            [-10,   5,   5,   5,   5,   5,   0, -10],
+            [-10,   0,   5,   0,   0,   0,   0, -10],
+            [-20, -10, -10,  -5,  -5, -10, -10, -20]
+        ]
+        
+        # King middlegame table - encourage castling and safety
+        self.king_middlegame_table = [
+            [-30, -40, -40, -50, -50, -40, -40, -30],
+            [-30, -40, -40, -50, -50, -40, -40, -30],
+            [-30, -40, -40, -50, -50, -40, -40, -30],
+            [-30, -40, -40, -50, -50, -40, -40, -30],
+            [-20, -30, -30, -40, -40, -30, -30, -20],
+            [-10, -20, -20, -20, -20, -20, -20, -10],
+            [ 20,  20,   0,   0,   0,   0,  20,  20],
+            [ 20,  30,  10,   0,   0,  10,  30,  20]
+        ]
+        
+        # King endgame table - encourage centralization
+        self.king_endgame_table = [
+            [-50, -40, -30, -20, -20, -30, -40, -50],
+            [-30, -20, -10,   0,   0, -10, -20, -30],
+            [-30, -10,  20,  30,  30,  20, -10, -30],
+            [-30, -10,  30,  40,  40,  30, -10, -30],
+            [-30, -10,  30,  40,  40,  30, -10, -30],
+            [-30, -10,  20,  30,  30,  20, -10, -30],
+            [-30, -30,   0,   0,   0,   0, -30, -30],
+            [-50, -30, -30, -30, -30, -30, -30, -50]
+        ]
     
+    def get_piece_square_value(self, piece_type, row, col, is_white, is_endgame):
+        """
+        Get the piece-square table value for a piece.
+        Black's tables are flipped vertically from White's.
+        """
+        # Flip row for black pieces
+        table_row = row if is_white else 7 - row
+        
+        if piece_type == PAWN:
+            return self.pawn_table[table_row][col]
+        elif piece_type == KNIGHT:
+            return self.knight_table[table_row][col]
+        elif piece_type == BISHOP:
+            return self.bishop_table[table_row][col]
+        elif piece_type == ROOK:
+            return self.rook_table[table_row][col]
+        elif piece_type == QUEEN:
+            return self.queen_table[table_row][col]
+        elif piece_type == KING:
+            if is_endgame:
+                return self.king_endgame_table[table_row][col]
+            else:
+                return self.king_middlegame_table[table_row][col]
+        
+        return 0
+
     def evaluate(self, board):
         """
         Evaluate the position from White's perspective.
