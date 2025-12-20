ecfcf769e32072623264bb166abd3dfa2f6c1e1d
kkoehler4070 <koehler.62@gmail.com>
2025-12-20 12:57:00 -0500

    Update test_opening_book.py
    modified tests

--- tests/test_opening_book.py
+++ tests/test_opening_book.py
@@ -3,14 +3,20 @@ from opening_book import OpeningBook
 from board import Board
 
 class TestOpeningBook(unittest.TestCase):
+    def setUp(self):
+        self.book = OpeningBook('books/kasparov.bin')
+        self.board = Board()
+
     def test_opening_book(self):
-        book = OpeningBook('books/kasparov.bin')
-        self.assertIsNotNone(book)
+        self.assertIsNotNone(self.book)
 
-        board = Board()
-        self.assertTrue(book.is_in_book())
+        self.assertTrue(self.book.is_in_book(self.board))
 
-        self.assertTrue(len(book.get_book_moves_info(board)) == 3)
+        self.assertTrue(len(self.book.get_book_moves_info(self.board)) == 3)
+
+    def test_get_move(self):
+        move = self.book.get_book_move(self.board,1,"best")
+        self.assertEqual(move,'e2e4')
 
 if __name__ == "__main__":
     unittest.main()
