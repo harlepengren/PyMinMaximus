addd91f57dc628d66bb70649ab6ebbb6a7ac3e54
kkoehler4070 <koehler.62@gmail.com>
2025-12-24 06:08:00 -0500

    Update test_performance.py
    Added more complicated fen

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -6,10 +6,11 @@ import time
 class PerformanceTest(unittest.TestCase):
     def test_alphabeta_speed(self):
         """Tests the nodes per second of minmaxalphabeta."""
+        fen = "8/8/8/2P3R1/5B2/2rP1p2/p1P1PP2/RnQ1K2k w Q - 0 20"
 
         board = Board()
+        board.from_fen(fen)
         engine = SearchEngine(board)
-        total_time = 0
 
         print("="*60)
         print("Testing MiniMaxAlpha Speed (depth=6)")
