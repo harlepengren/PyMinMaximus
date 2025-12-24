9eb82c1ddc79379b12dbb4fd82c81978d4c1db72
kkoehler4070 <koehler.62@gmail.com>
2025-12-24 06:00:47 -0500

    Update test_performance.py
    Added performance test for alpha beta.

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -1,8 +1,21 @@
 import unittest
 from board import Board
 from search import SearchEngine
+import time
 
 class PerformanceTest(unittest.TestCase):
-    def test_minmaxalphabeta_speed(self):
+    def test_minimaxalphabeta_speed(self):
         """Tests the nodes per second of minmaxalphabeta."""
 
+        board = Board()
+        engine = SearchEngine(board)
+        total_time = 0
+
+        print("="*60)
+        print("Testing MiniMaxAlpha Speed (depth=6)")
+        start = time.perf_counter()
+        bestmove, eval = engine.find_best_move_alphabeta(6)
+        end = time.perf_counter()
+        print(f"Best Move Identified: {bestmove} with score of {eval}")
+        print(f"Total time: {end-start}")
+        print(f"Nodes/Second (nps): {engine.nodes_searched/(end-start)}")
