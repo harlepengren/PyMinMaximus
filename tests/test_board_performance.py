2a2d5422329e1ec5f04a841001316eb9572bae64
kkoehler4070 <koehler.62@gmail.com>
2025-12-25 05:32:01 -0500

    Update test_board_performance.py
    Created utility function to reduce code.

--- tests/test_board_performance.py
+++ tests/test_board_performance.py
@@ -3,52 +3,57 @@ from board import Board
 import time
 from constants import *
 
+def get_piece_moves(board, target_piece):
+    num_pieces = 0
+    moves = []
+
+    for row in range(8):
+            for col in range(8):
+                piece = board.board[row][col]
+                
+                if piece == EMPTY or (piece & 24) != board.to_move:
+                    continue
+                
+                piece_type = piece & 7
+                if piece_type != target_piece:
+                    continue
+                
+                if piece_type == PAWN:
+                    board.generate_pawn_moves(row, col, moves)
+                elif piece_type == KNIGHT:
+                    board.generate_knight_moves(row, col, moves)
+                elif piece_type == BISHOP:
+                    board.generate_bishop_moves(row, col, moves)
+                elif piece_type == ROOK:
+                    board.generate_rook_moves(row, col, moves)
+                elif piece_type == QUEEN:
+                    board.generate_queen_moves(row, col, moves)
+                elif piece_type == KING:
+                    board.generate_king_moves(row, col, moves)
+                num_pieces += 1
+
+    return num_pieces, len(moves)
+
 class TestBoardPerformance(unittest.TestCase):
     def test_pawns(self):
-        pawn_counter = 0
-        moves = []
-
         board = Board()
         start = time.perf_counter()
         for _ in range(10000):
-            for row in range(8):
-                for col in range(8):
-                    piece = board.board[row][col]
-                
-                    if piece == EMPTY or (piece & 24) != board.to_move:
-                        continue
-                
-                    piece_type = piece & 7
-                    if piece_type == PAWN:
-                        board.generate_pawn_moves(row,col,moves)
-                        pawn_counter += 1
+            pawn_counter, num_moves = get_piece_moves(board,PAWN)
         end = time.perf_counter()
         print("="*60)
         print("Pawn Move Generation Test")
         print(f"{pawn_counter} pawns tested")
-        print(f"{len(moves)} moves generated in {end-start} seconds")
+        print(f"{num_moves} moves generated in {end-start} seconds")
 
     def test_bishops(self):
-        bishop_counter = 0
-        moves = []
-
         board = Board()
         board.from_fen('8/2b1k3/6b1/8/2B5/8/3K1B2/8 w - - 0 1')
         start = time.perf_counter()
         for _ in range(10000):
-            for row in range(8):
-                for col in range(8):
-                    piece = board.board[row][col]
-                
-                    if piece == EMPTY or (piece & 24) != board.to_move:
-                        continue
-                
-                    piece_type = piece & 7
-                    if piece_type == BISHOP:
-                        board.generate_bishop_moves(row,col,moves)
-                        bishop_counter += 1
+            bishop_counter, num_moves = get_piece_moves(board,BISHOP)
         end = time.perf_counter()
         print("="*60)
         print("Bishop Move Generation Test")
         print(f"{bishop_counter} bishops tested")
-        print(f"{len(moves)} moves generated in {end-start} seconds")
+        print(f"{num_moves} moves generated in {end-start} seconds")
