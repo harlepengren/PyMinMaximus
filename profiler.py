a02d6bfb1e51643bc1f5a419232e495e44db03fb
kkoehler4070 <koehler.62@gmail.com>
2025-12-31 08:58:59 -0500

    Update profiler.py

--- profiler.py
+++ profiler.py
@@ -1,20 +1,24 @@
 import cProfile
 import pstats
+from io import StringIO
 
 from board import Board
 from search import SearchEngine
 
-def run_profile():
-    board = Board()
-    board.from_fen("rnbqkbnr/pp2ppp1/2p4p/6B1/3Pp3/2N5/PPP2PPP/R2QKBNR w KQkq - 0 5")
-    engine = SearchEngine(board)
+board = Board()
+board.from_fen("rnbqkbnr/pp2ppp1/2p4p/6B1/3Pp3/2N5/PPP2PPP/R2QKBNR w KQkq - 0 5")
+engine = SearchEngine(board)
 
-    profiler = cProfile.Profile()
-    profiler.enable()
+profiler = cProfile.Profile()
+profiler.enable()
 
-    move, score = engine.find_best_move_alphabeta(6)
+move, score = engine.find_best_move_alphabeta(5)  # depth 5 for faster profiling
 
-    profiler.disable()
-    stats = pstats.Stats(profiler)
-    stats.sort_stats('cumulative')
-    stats.print_stats(20)  # Top 20 functions
+profiler.disable()
+
+# Print to string so we can see it all
+s = StringIO()
+stats = pstats.Stats(profiler, stream=s)
+stats.sort_stats('cumulative')
+stats.print_stats(30)
+print(s.getvalue())
